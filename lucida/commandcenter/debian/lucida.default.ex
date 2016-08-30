65b3ce22d332f2fcd0f2e9125662b2c6265a7608
LLuca Vercelli <luca.vercelli.to@gmail.com>
2016-08-30 18:09:09 +0200

    basic debian support

---
+++ lucida/commandcenter/debian/lucida.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lucida initscript
+# sourced by /etc/init.d/lucida
+# installed at /etc/default/lucida by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
